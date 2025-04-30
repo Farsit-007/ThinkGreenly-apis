cc6f7c060afca13ba4d42c1d264a31fa9de9bb2a
Rakib <khandokerrakib.hasan@bracits.com>
2025-04-30 13:46:16 +0600

    fix: handle error file fix

--- src/app/utils/handleErrors.ts
+++ src/app/utils/handleErrors.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from 'express';
+import { httpStatus } from './httpStatus';
 
 const handleErrors = (
   err: any,
