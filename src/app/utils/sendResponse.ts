45a27860948bfff514da3ac684118a3a440fe091
Md Rashedul Islam <rajib5570@gmail.com>
2025-04-30 13:26:14 +0600

    [REFACTOR]: utilities/sendResponse.ts modified for optional meta data

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,18 +1,37 @@
-import { Response } from "express";
+import type { Response } from 'express';
 
-type TResponse<T> = {
-    success: boolean;
-    message?: string;
-    statusCode: number;
-    data?: T;
-};
-
-const sendResponse = <T>(res: Response, data: TResponse<T>) => {
-    res.status(data.statusCode).json({
-        success: data.success,
-        message: data?.message,
-        data: data?.data,
+/**
+ *
+ * @param res Response from Express.js from the Specific Controller
+ * @param statusCode HTTP Status Code
+ * @param success Success Response as boolean (`true` or `false`)
+ * @param message Custom Message Message
+ * @param meta Optional meta data for pagination
+ * @param data Optional Data to send
+ */
+const sendResponse = <T>(
+  res: Response,
+  statusCode: number,
+  success: boolean,
+  message: string,
+  meta?: { page: number; limit: number; total: number },
+  data?: T
+): void => {
+  if (data) {
+    res.status(statusCode).json({
+      success,
+      message,
+      statusCode,
+      meta,
+      data,
     });
+  } else {
+    res.status(statusCode).json({
+      success,
+      message,
+      statusCode,
+    });
+  }
 };
 
 export default sendResponse;
