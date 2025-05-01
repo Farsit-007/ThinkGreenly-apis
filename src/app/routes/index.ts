d6911648f4a232ab3bc38288594bcb3e5f9392a0
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-05-01 00:58:22 -0700

    commit

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
 import IdeaRoutes from '../modules/idea/idea.route';
 import CategoryRoutes from '../modules/category/category.route';
-<<<<<<< HEAD
 import CommentsRoutes from '../modules/comments/comments.routes';
-=======
 import { adminRoutes } from '../modules/Admin/admin.route';
 import { userRoutes } from '../modules/Users/user.route';
 import { authRoutes } from '../modules/Auth/auth.route';
->>>>>>> d8a46ade14d07b0c1308e6abee9e9e99de7bc0c0
 
 const router = Router();
 
@@ -29,14 +26,11 @@ const moduleRoutes = [
     route: CategoryRoutes,
   },
   {
-<<<<<<< HEAD
     path: '/comments',
     route: CommentsRoutes,
-=======
-    path: '/admin',
-    route: adminRoutes,
->>>>>>> d8a46ade14d07b0c1308e6abee9e9e99de7bc0c0
   },
+  {path: '/admin',
+  route: adminRoutes,}
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
