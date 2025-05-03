71ce5bbe6dfdde393122a22aa348b00bd5f9cb20
khaledssbd <khaledssbd@gmail.com>
2025-05-04 04:51:03 +0600

    cloudinaryImageUploader fixed

--- src/app/utils/cloudinaryImageUploader.ts
+++ src/app/utils/cloudinaryImageUploader.ts
@@ -1,40 +1,38 @@
+import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 import multer from 'multer';
-import { v2 as cloudinary, type UploadApiResponse } from 'cloudinary';
-import dotenv from 'dotenv';
-/**
- *
- * @param storage storage for holding file using multer
- * @param uploadFile uploader function for image files to storage
- * @param sendImageToCloudinary uploader function for images files to cloudinary
- **/
-dotenv.config();
+import { Readable } from 'stream';
+import config from '../config';
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
-  api_key: process.env.CLOUDINARY_API_KEY!,
-  api_secret: process.env.CLOUDINARY_API_SECRET!,
+  cloud_name: config.cloudinary_cloud_name,
+  api_key: config.cloudinary_api_key,
+  api_secret: config.cloudinary_api_secret,
 });
 
-// Multer configuration for memory storage
-const storage = multer.memoryStorage();
-export const uploadFile = multer({ storage });
-
-// Upload file to Cloudinary directly from memory
 export const sendImageToCloudinary = (
   imageName: string,
   buffer: Buffer
-): Promise<UploadApiResponse> => {
+): Promise<Record<string, unknown>> => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { public_id: imageName?.trim() },
+      { public_id: imageName.trim() },
       (error, result) => {
         if (error) {
-          return reject(error);
+          reject(error);
+        } else {
+          resolve(result as UploadApiResponse);
         }
-        resolve(result as UploadApiResponse);
       }
     );
-    uploadStream.end(buffer);
+
+    const readableStream = new Readable();
+    readableStream.push(buffer);
+    readableStream.push(null);
+    readableStream.pipe(uploadStream);
   });
 };
+
+const storage = multer.memoryStorage();
+
+export const uploadFile = multer({ storage: storage });
